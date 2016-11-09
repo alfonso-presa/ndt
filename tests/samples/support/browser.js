ecd64f421b2ada51a419862341eed3ca44ef2166
Alfonso Presa <alfonso.presa@gmail.com>
2016-11-09 12:59:33 +0100

    feat(): Allow listen returns promise

--- tests/samples/support/browser.js
+++ tests/samples/support/browser.js
@@ -16,7 +16,8 @@ module.exports = function () {
                         height: 800
                     },false)
                     .url('https://duckduckgo.com/')
-                browserBootstrapPromise.pause(50)
+                browserBootstrapPromise
+                    .pause() //Needed to make next step async
 
                     .then(() => observe.trigger('home'))
 
@@ -38,64 +39,64 @@ module.exports = function () {
         });
     }
 
-    this.When(/^I'm in the wellcome screen$/, (callback) => {
-        observe.listen('home', callback);
-    });
+    this.When(/^I'm in the wellcome screen$/, () =>
+        observe.listen('home')
+    );
 
-    this.Then(/^I should see the search button$/, () => {
-        return observe.listen('*', () => {
-            return browser.isVisible('#search_button_homepage').then((visible) => {
-                return visible || browser.isVisible('#search_button');
-            }).should.eventually.be.true
-        });
-    });
+    this.Then(/^I should see the search button$/, () =>
+        observe.listen('*', () =>
+            browser.isVisible('#search_button_homepage').then((visible) =>
+                visible || browser.isVisible('#search_button')
+            ).should.eventually.be.true
+        )
+    )
 
-    this.When(/^I search '(.*)'$/, (keyword, callback) => {
-        observe.listen('searched ' + keyword, callback);
-    });
+    this.When(/^I search '(.*)'$/, (keyword) =>
+        observe.listen('searched ' + keyword)
+    );
 
-    this.When(/^I search anything$/, (callback) => {
-        observe.listen('searched', callback);
-    });
+    this.When(/^I search anything$/, () =>
+        observe.listen('searched')
+    );
 
-    this.When(/^I search a company$/, (callback) => {
-        observe.listen('searched company', callback);
-    });
+    this.When(/^I search a company$/, () =>
+        observe.listen('searched company')
+    );
 
-    this.Then(/^I should see the results for (.*)$/, (keyword) => {
-        return observe.listen('searched ' + keyword, () => {
-            return browser.getText('.zci--meanings .metabar__primary-text')
-                .should.eventually.equal('Resultados para ' + keyword);
-        });
-    });
+    this.Then(/^I should see the results for (.*)$/, (keyword) =>
+        observe.listen('searched ' + keyword, () =>
+            browser.getText('.zci--meanings .metabar__primary-text')
+                .should.eventually.equal('Resultados para ' + keyword)
+        )
+    )
 
-    this.Then(/^I should see the search tabs$/, () => {
-        return observe.listen('searched', () => {
-            return browser.getText('.zcm__item')
-                .should.eventually.include.members(['Web','Imágenes','Videos']);
-        });
-    });
+    this.Then(/^I should see the search tabs$/, () =>
+        observe.listen('searched', () =>
+            browser.getText('.zcm__item')
+                .should.eventually.include.members(['Web','Imágenes','Videos'])
+        )
+    );
 
-    this.Then(/^I should see '(.*)' in the tabs$/, (title) => {
-        return observe.listen('searched', () => {
-            return browser.getText('.zcm__item')
-                .should.eventually.include(title);
-        });
-    });
+    this.Then(/^I should see '(.*)' in the tabs$/, (title) =>
+        observe.listen('searched', () =>
+            browser.getText('.zcm__item')
+                .should.eventually.include(title)
+        )
+    );
 
-    this.Then(/^I should not see the text results for (.*)$/, (keyword) => {
-        return observe.listen('searched ' + keyword, () => {
-            return browser.isVisible('.zci--meanings .metabar__primary-text')
+    this.Then(/^I should not see the text results for (.*)$/, (keyword) =>
+        observe.listen('searched ' + keyword, () =>
+            browser.isVisible('.zci--meanings .metabar__primary-text')
                 .should.eventually.be.false
-        });
-    });
+        )
+    );
 
-    this.Then(/^I should see company info for (.*)$/, (keyword) => {
-        return observe.listen('searched company', () => {
-            return browser.getText('.c-info__title')
-                .should.eventually.equal(keyword);
-        });
-    });
+    this.Then(/^I should see company info for (.*)$/, (keyword) =>
+        observe.listen('searched company', () =>
+            browser.getText('.c-info__title')
+                .should.eventually.equal(keyword)
+        )
+    );
 
 
 }
