6fb31932141a2c5d5f0c71cc6d089796723dec88
Alfonso Presa <alfonso.presa@gmail.com>
2016-11-09 12:26:21 +0100

    refactor(): Code clean up

--- tests/samples/support/browser.js
+++ tests/samples/support/browser.js
@@ -7,7 +7,7 @@ module.exports = function () {
     {
         var browserBootstrapPromise;
 
-        this.Given(/^I perform duckduckgo navigation$/, {timeout:20000}, function () {
+        this.Given(/^I perform duckduckgo navigation$/, {timeout:20000}, () => {
             if(!browserBootstrapPromise) {
                 browserBootstrapPromise = browser
                     .init()
@@ -16,100 +16,84 @@ module.exports = function () {
                         height: 800
                     },false)
                     .url('https://duckduckgo.com/')
-                browserBootstrapPromise
-                    .pause(50)
-                    .then(function () {
-                        return observe.trigger('home');
-                    })
+                browserBootstrapPromise.pause(50)
+
+                    .then(() => observe.trigger('home'))
+
                     .setValue('#search_form_input_homepage', 'prueba')
-                    .then(function () {
-                        return observe.trigger('input prueba');
-                    })
+                    .then(() => observe.trigger('input prueba'))
+
                     .click('#search_button_homepage')
-                    .then(function () {
-                        return observe.trigger('searched prueba');
-                    })
+                    .then(() => observe.trigger('searched prueba'))
+
                     .setValue('#search_form_input', 'google')
-                    .then(function () {
-                        return observe.trigger('input google');
-                    })
+                    .then(() => observe.trigger('input google'))
+
                     .click('#search_button')
-                    .then(function () {
-                        return observe.trigger('searched','searched company','searched google')
-                    })
+                    .then(() => observe.trigger('searched','searched company','searched google'))
+
                     .end();
             }
             return browserBootstrapPromise;
         });
     }
 
-    this.When(/^I'm in the wellcome screen$/, function (callback) {
+    this.When(/^I'm in the wellcome screen$/, (callback) => {
         observe.listen('home', callback);
     });
 
-    this.Then(/^I should see the search button$/, function (callback) {
-        observe.listen('*', function () {
-            return browser.isVisible('#search_button_homepage').then(function (visible) {
+    this.Then(/^I should see the search button$/, () => {
+        return observe.listen('*', () => {
+            return browser.isVisible('#search_button_homepage').then((visible) => {
                 return visible || browser.isVisible('#search_button');
             }).should.eventually.be.true
-            .then(() => callback())
-            .catch(callback);
         });
     });
 
-    this.When(/^I search '(.*)'$/, function (keyword, callback) {
+    this.When(/^I search '(.*)'$/, (keyword, callback) => {
         observe.listen('searched ' + keyword, callback);
     });
 
-    this.When(/^I search anything$/, function (callback) {
+    this.When(/^I search anything$/, (callback) => {
         observe.listen('searched', callback);
     });
 
-    this.When(/^I search a company$/, function (callback) {
+    this.When(/^I search a company$/, (callback) => {
         observe.listen('searched company', callback);
     });
 
-    this.Then(/^I should see the results for (.*)$/, function (keyword, callback) {
-        observe.listen('searched ' + keyword, function () {
+    this.Then(/^I should see the results for (.*)$/, (keyword) => {
+        return observe.listen('searched ' + keyword, () => {
             return browser.getText('.zci--meanings .metabar__primary-text')
-                .should.eventually.equal('Resultados para ' + keyword)
-                .then(() => callback());
+                .should.eventually.equal('Resultados para ' + keyword);
         });
     });
 
-    this.Then(/^I should see the search tabs$/, function (callback) {
-        observe.listen('searched', function () {
+    this.Then(/^I should see the search tabs$/, () => {
+        return observe.listen('searched', () => {
             return browser.getText('.zcm__item')
-                .should.eventually.include.members(['Web','Imágenes','Videos'])
-                .then(() => callback())
-                .catch(callback);
+                .should.eventually.include.members(['Web','Imágenes','Videos']);
         });
     });
 
-    this.Then(/^I should see '(.*)' in the tabs$/, function (title, callback) {
-        observe.listen('searched', function () {
+    this.Then(/^I should see '(.*)' in the tabs$/, (title) => {
+        return observe.listen('searched', () => {
             return browser.getText('.zcm__item')
-                .should.eventually.include(title)
-                .then(() => callback())
-                .catch(callback);
+                .should.eventually.include(title);
         });
     });
 
-    this.Then(/^I should not see the text results for (.*)$/, function (keyword, callback) {
-        observe.listen('searched ' + keyword, function () {
+    this.Then(/^I should not see the text results for (.*)$/, (keyword) => {
+        return observe.listen('searched ' + keyword, () => {
             return browser.isVisible('.zci--meanings .metabar__primary-text')
                 .should.eventually.be.false
-                .then(() => callback());
         });
     });
 
-    this.Then(/^I should see company info for (.*)$/, function (keyword, callback) {
-        observe.listen('searched company', function () {
-            console.log('testing');
+    this.Then(/^I should see company info for (.*)$/, (keyword) => {
+        return observe.listen('searched company', () => {
             return browser.getText('.c-info__title')
-                .should.eventually.equal(keyword)
-                .then(() => callback())
-                .catch(callback);
+                .should.eventually.equal(keyword);
         });
     });
 
