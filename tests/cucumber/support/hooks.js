4356a8266759668aa79d11e3f05a3ba2433ff247
Alfonso Presa <alfonso.presa@gmail.com>
2016-11-11 17:37:15 +0100

    refactor(): Extracted browser intialization

--- tests/cucumber/support/hooks.js
+++ tests/cucumber/support/hooks.js
@@ -2,23 +2,6 @@
 
 module.exports = function (){
 
-    var webdriverio = require('webdriverio');
-    var options = {
-        desiredCapabilities: {
-            browserName: 'chrome',
-            chromeOptions: {
-                binary: /darwin/.test(process.platform) && '/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'
-            }
-        }
-    };
-    var chai = require('chai');
-    var chaiAsPromised = require('chai-as-promised');
-
-    var client = webdriverio.remote(options);
-    chai.Should();
-    chai.use(chaiAsPromised);
-    chaiAsPromised.transferPromiseness = client.transferPromiseness;
-
-    global.browser = client;
+    global.browser = require('../../tools/browser.js')();
 
 };
---
+++ tests/tools/browser.js
@@ -0,0 +1,21 @@
+var webdriverio = require('webdriverio');
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+
+module.exports = function () {
+    var options = {
+        desiredCapabilities: {
+            browserName: 'chrome',
+            chromeOptions: {
+                binary: /darwin/.test(process.platform) && '/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'
+            }
+        }
+    };
+
+    var client = webdriverio.remote(options);
+    chai.Should();
+    chai.use(chaiAsPromised);
+    chaiAsPromised.transferPromiseness = client.transferPromiseness;
+
+    return client;
+}
