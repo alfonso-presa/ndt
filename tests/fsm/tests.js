151d5e6cfbf8c4ae739b093e3b657961cf782d5e
Alfonso Presa <alfonso.presa@gmail.com>
2016-11-12 00:20:07 +0100

    refactor(): Better fsm test structure

--- tests/fsm/tests.js
+++ tests/fsm/tests.js
@@ -1,37 +1,76 @@
 'use strict';
 
+require('colors');
+
 let browser = require('../tools/browser')();
 let navigation = new (require('../observables/sample-navigation'))();
 let suite = require('../tools/suite');
 
-const _ = undefined; //Undefined shortcut
+let testSuite = {
 
-var specs = {
+    scenarios: {
 
-    'Should see Acerca De for company': {
-        'home':_,
-        'searched company': () =>
-            browser
-                .getText('.zcm__item')
-                .should.eventually.include('Acerca De')
+        'Search button': [
+            'When I\'m in the wellcome screen',
+            'Then I should see the search button'
+        ],
+
+        'Simple search': [
+            'When I search "prueba"',
+            'Then I should see the results for prueba',
+        ],
+
+        'Search tabs': [
+            'When I search anything',
+            'Then I should see the search tabs',
+            'And I should see the search button'
+        ],
+
+        'Search tabs for company': [
+            'When I search a company',
+            'Then I should see "AcercaDe" in the tabs',
+        ],
+
+        'Company search': [
+            'When I search "google"',
+            'Then I should not see the text results for Google',
+            'And I should see company info for google',
+        ]
+
     },
 
-    'Should see results for prueba': {
-        'home': _,
-        'searched prueba': () =>
-            browser
-                .getText('.zci--meanings .metabar__primary-text')
-                .should.eventually.equal('Resultados para prueba')
+    triggers: {
+        'I\'m in the wellcome screen': 'home',
+        'I search "(.*)"': 'searched $1',
+        'I search a company': 'searched company',
+        'I search anything': 'searched'
     },
 
-    'Should see search button': {
-        '*': () =>
+    assertions: {
+
+        'I should see the search button': () =>
             browser.isVisible('#search_button_homepage').then((visible) =>
                 visible || browser.isVisible('#search_button')
-            ).should.eventually.be.true
+            ).should.eventually.be.true,
+
+        'I should see the results for (.*)': (keyword) =>
+            browser.getText('.zci--meanings .metabar__primary-text')
+                .should.eventually.equal('Resultados para ' + keyword),
+
+        'I should see the search tabs': () =>
+            browser.getText('.zcm__item')
+                .should.eventually.include.members(['Web','Imágenes','Videos']),
+
+        'I should see "(.*)" in the tabs': (title) =>
+            browser.getText('.zcm__item')
+                .should.eventually.include(title),
+
+        'I should not see the text results for (.*)': (keyword) =>
+            browser.getText('.c-info__title')
+                .should.eventually.equal(keyword),
+
     }
 
-};
+}
 
-suite.attach(specs, navigation, navigation.start(browser));
-
+suite.attach(testSuite, navigation, navigation.start(browser));
--- tests/tools/suite.js
+++ tests/tools/suite.js
@@ -2,42 +2,95 @@
 
 require('colors');
 
-function attachSuite(suite, navigation, stack) {
-    for(var test in suite) {
-        var spec = suite[test];
-        attachTest(test, spec, navigation, stack);
-    }
-}
-
-function attachTest(test, spec, navigation, stack) {
-    spec = Array.isArray(spec) ? spec : [spec];
-    return spec.reduce((stack, step) =>
-            attachStep(step, navigation, stack)
-        , stack)
-        .then(() => console.log('[OK] '.green + test))
-        .catch((e) => console.error('[KO] '.red + test + '\n' + e));
-}
-
-function attachStep(step, navigation, stack, callback) {
-    function reporter(step, callback) {
-        return function () {
-            console.log(('\t[' + step + ']').yellow + ' Reached');
-            return callback ? callback() : undefined;
-        };
-    }
-
-    if (typeof step === 'string' || step instanceof String) {
-        return stack.then(() => navigation.listen(step, reporter(step, callback)));
-    } else {
-        for(var event in step) {
-            stack = attachStep(event, navigation, stack, step[event]);
-        }
-        return stack;
-    }
-}
-
 module.exports = {
     attach: function (suite, navigation, tracker) {
-        attachSuite(suite, navigation, tracker);
+        let state = undefined;
+
+        let stepToEvent = suite.triggers;
+        let stepToAsertion = suite.assertions;
+        let scenarios = suite.scenarios;
+
+        function processScenarios(scenarios, navigation, stack) {
+            for(let scenario in scenarios) {
+                state = undefined;
+                (function (scenario) {
+                    let steps = scenarios[scenario];
+                    steps.reduce((stack,step) =>
+                        processStep(step, navigation, stack)
+                    ,stack)
+                        .then(() => console.log('[OK] '.green + scenario))
+                        .catch((e) => console.error('[KO] '.red + scenario + '\n' + e));
+                })(scenario);
+            }
+        }
+
+        function processStep(step, navigation, stack) {
+            function reporter(step, callback) {
+                return function () {
+                    console.log(('\t[' + step + ']').yellow + ' Reached');
+                    if(callback) {
+                        return callback().catch((e) => {
+                            console.error(('\t[' + step + ']').yellow + ' Failed'.red);
+                            throw e;
+                        });
+                    }
+                };
+            }
+
+            if(step.length < 6){
+                throw 'Invalid step ' + step;
+            }
+
+            let pos = step.indexOf(' ');
+            let type = step.substring(0, pos).toLowerCase();
+            let text = step.substring(pos +1);
+
+            if(type === 'and') {
+                type = state;
+            }
+            state = type;
+
+            if(type === 'when') {
+                return stack.then(() =>
+                    navigation.listen(
+                        findElement(stepToEvent, step),
+                        reporter(step)
+                    )
+                );
+            } else if(type === 'then') {
+                return stack.then(() =>
+                    navigation.listen(
+                        '*',
+                        reporter(step, findElement(stepToAsertion, step))
+                    )
+                );
+            } else {
+                throw 'Invalid step ' + step;
+            }
+        }
+
+        function findElement(list, text) {
+            for(let name in list) {
+                let matcher = new RegExp(name);
+                let matches = matcher.exec(text);
+                if(matches) {
+                    let rt = list[name];
+                    if(typeof rt === 'string' || rt instanceof String) {
+                        for(let i = 1; i < matches.length; i++) {
+                            rt = rt.replace(new RegExp('\\$' + i, 'g'), matches[i]);
+                        }
+                    } else {
+                        matches.shift();
+                        let fn = rt;
+                        rt = function () {
+                            return fn.apply(undefined, matches);
+                        }
+                    }
+                    return rt;
+                }
+            }
+        }
+
+        processScenarios(scenarios, navigation, tracker);
     }
 }
