6aed7bce012babc911505943b44df1f185c11263
Alfonso Presa <alfonso.presa@gmail.com>
2016-11-13 22:40:21 +0100

    refactor(navigation): A better code layout

--- tests/observables/sample-navigation.js
+++ tests/observables/sample-navigation.js
@@ -2,10 +2,8 @@
 
 let Navigation = require('../../lib/navigation');
 
-module.exports = function () {
-
-    let walkThrough =
-        `
+module.exports = () => new Navigation(
+       `
             Init
             Search word ejemplo
             Switch to images tab
@@ -15,33 +13,27 @@ module.exports = function () {
             Switch to web tab
             Search word prueba
             Search company pepito
+        `, {
 
-        `;
+            'Init': (browser, navigator) => browser
+                .url('https://duckduckgo.com/')
+                .then(() => navigator.observable.setStatus('page','home'))
+                .then(() => navigator.observable.trigger('navigate')),
 
-    let actions = {
+            'Search (.*) (.*)': (browser, navigator, type, text) => browser
+                .setValue('.js-search-input', text)
+                .click('.search__button')
+                .then(() => navigator.observable.setStatus('text',text))
+                .then(() => navigator.observable.setStatus('type',type))
+                .then(() => {
+                    if(navigator.observable.getStatus('page') === 'home') {
+                        return navigator.observable.setStatus('page', 'web');
+                    }
+                })
+                .then(() => navigator.observable.trigger('searched', type + ' search', 'navigate')),
 
-        'Init': (browser, navigator) => browser
-            .url('https://duckduckgo.com/')
-            .then(() => navigator.observable.setStatus('page','home'))
-            .then(() => navigator.observable.trigger('navigate')),
+            'Switch to (.*) tab': (browser, navigator, tab) => browser
+                .then(() => navigator.observable.setStatus('page', tab))
+                .then(() => navigator.observable.trigger('navigate'))
 
-        'Search (.*) (.*)': (browser, navigator, type, text) => browser
-            .setValue('.js-search-input', text)
-            .click('.search__button')
-            .then(() => navigator.observable.setStatus('text',text))
-            .then(() => navigator.observable.setStatus('type',type))
-            .then(() => {
-                if(navigator.observable.getStatus('page') === 'home') {
-                    return navigator.observable.setStatus('page', 'web');
-                }
-            })
-            .then(() => navigator.observable.trigger('searched', type + ' search', 'navigate')),
-
-        'Switch to (.*) tab': (browser, navigator, tab) => browser
-            .then(() => navigator.observable.setStatus('page', tab))
-            .then(() => navigator.observable.trigger('navigate'))
-
-    };
-
-    return new Navigation(walkThrough, actions);
-}
+    })
